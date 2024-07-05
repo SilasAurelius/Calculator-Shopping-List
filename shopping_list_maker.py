b5975721cfa9c31b6f6e1baaf4d44786cf8134b8
Silas Aurelius <162638735+SilasAurelius@users.noreply.github.com>
2024-07-05 19:33:51 -0400

    shopping_list_maker.py

--- shopping_list_maker.py
+++ shopping_list_maker.py
@@ -1,31 +1,32 @@
+#Task 1: Write a function that lets the user add items to a list.
 shopping_list = []
 
 def add_item(item):
+    global shopping_list
     shopping_list.append(item)
     print(f"{item} added to list.")
-    
+# Task 2: Include a function to remove items from the list.    
 def remove_item(item):
+    global shopping_list
     shopping_list.remove(item)
     print(f"{item} removed from list.")
-    
+# Task 3: Add a function that prints out the entire list in a formatted way.    
 def print_list():
     print("Current shopping list:")
     for i, item in enumerate(shopping_list, 1):
         print(f"{i}. {item}")
  
 while True: 
-	manage_list = input("Type: + to add, - to remove, c to check list, q to quit:: ")
-	if manage_list == "+":
-		item = input("Input item to add: ")
-		add_item(item)
-	elif manage_list == "-":
-		item = input("Input item to remove: ")
-		remove_item(item)
-	elif manage_list == "c":
-		print_list()
-	else:
-		print(f"Final List: {print_list(shopping_list)}")
-		break
-    
-     
- 
+    manage_list = input("Type: + to add, - to remove, q to quit:: ")
+    if manage_list == "+":
+        item = input("Input item to add: ")
+        add_item(item)
+    elif manage_list == "-":
+        item = input("Input item to remove: ")
+        remove_item(item)
+    elif manage_list == "q":
+        print_list()
+        print("Exiting program.")
+        break
+    else:
+        print("Invalid input. Please try again.")
